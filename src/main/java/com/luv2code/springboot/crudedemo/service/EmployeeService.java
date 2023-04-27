128fca9de2b7d14d389150e009a82b3fb2901ea2
BhargaviRamanaVaka <vakabhargavi15@gmail.com>
2023-04-27 10:10:17 +0530

    weather

---
+++ src/main/java/com/luv2code/springboot/crudedemo/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.crudedemo.service;
+
+import com.luv2code.springboot.crudedemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+    void deleteById(int theId);
+}
